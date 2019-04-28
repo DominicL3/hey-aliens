aa10d0b9024a019ec4481fb2449d546cd1c1316a
DominicL3 <leducd98@gmail.com>
2019-04-28 00:39:57 -0700

    reduced layer size

--- simulateFRBclassification/simulated_NN.py
+++ simulateFRBclassification/simulated_NN.py
@@ -254,10 +254,12 @@ def construct_conv2d(train_data, train_labels, eval_data, eval_labels,
 
     # flatten all neurons and run through fully connected layers
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.3))
+    
     model.add(Dense(32, activation='relu'))
-    model.add(Dropout(0.2))
+    model.add(Dense(16, activation='relu'))
+    
 
     # output probabilities of predictions and choose the maximum
     model.add(Dense(2, activation='softmax'))
