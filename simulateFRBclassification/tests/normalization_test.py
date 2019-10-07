fb1e936bd46a763ac00089f8d632945ba9755f9a
DominicL3 <leducd98@gmail.com>
2019-10-06 18:18:54 -0700

    test more used in interactive python

--- simulateFRBclassification/tests/normalization_test.py
+++ simulateFRBclassification/tests/normalization_test.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
 import psr2np
 import sys
 
@@ -19,12 +18,7 @@ def extract_DM(fname):
     dm = fpsr.get_dispersion_measure()
     return dm
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        filename = str(sys.argv[1])
-    else:
-        raise RuntimeError('Must pass in only filename to .ar file to test')
-    
+def normalization_plot(fname):
     # dedisperse the file and convert to numpy array
     dm = extract_DM(filename)
     data = psr2np.psr2np(fname=filename, NCHAN=64, dm=dm)[0]
@@ -32,11 +26,11 @@ if __name__ == "__main__":
     # extract the time data and frequency data
     time_data = np.sum(data, axis=0)
     freq_data = np.sum(data, axis=1)
-    
+
     # plot spectrogram, 1D pulse with time, and 1D pulse in time
     plt.ion()
     fig, ax = plt.subplots(nrows=3, ncols=1)
-    
+
     ax[0].plot(time_data)
     ax[0].set_title('Time')
     ax[1].imshow(data)
