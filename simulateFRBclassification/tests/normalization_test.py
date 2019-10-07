99d7551216a0083ca59df8b248c48edf91ce3f18
DominicL3 <leducd98@gmail.com>
2019-10-06 18:25:53 -0700

    also generate normalization plot

--- simulateFRBclassification/tests/normalization_test.py
+++ simulateFRBclassification/tests/normalization_test.py
@@ -18,7 +18,7 @@ def extract_DM(fname):
     dm = fpsr.get_dispersion_measure()
     return dm
 
-def normalization_plot(fname):
+def norm_plot(fname):
     # dedisperse the file and convert to numpy array
     dm = extract_DM(fname)
     data = psr2np.psr2np(fname=fname, NCHAN=64, dm=dm)[0]
@@ -29,12 +29,27 @@ def normalization_plot(fname):
 
     # plot spectrogram, 1D pulse with time, and 1D pulse in time
     plt.ion()
-    fig, ax = plt.subplots(nrows=3, ncols=1)
+    fig_original, ax_original = plt.subplots(nrows=3, ncols=1)
 
-    ax[0].plot(time_data)
-    ax[0].set_title('Time')
-    ax[1].imshow(data, aspect='auto')
-    ax[2].plot(freq_data)
-    ax[2].set_title('Frequency')
+    ax_original[0].plot(time_data)
+    ax_original[0].set_title('Time')
+    ax_original[1].imshow(data, aspect='auto')
+    ax_original[2].plot(freq_data)
+    ax_original[2].set_title('Frequency')
     
-    fig.tight_layout()
+    fig_original.tight_layout()
+
+    # normalize data and plot
+    norm_data = psr2np.normalize_background(data)
+    time_norm = np.sum(data, axis=0)
+    freq_norm = np.sum(data, axis=1)
+
+    fig_norm, ax_norm = plt.subplots(nrows=3, ncols=1)
+
+    ax_norm[0].plot(time_norm)
+    ax_norm[0].set_title('Normalized Time')
+    ax_norm[1].imshow(norm_data, aspect='auto')
+    ax_norm[2].plot(freq_norm)
+    ax_norm[2].set_title('Normalized Frequency')
+    
+    fig_norm.tight_layout()
