92114c8eae7f4d428579384e7d2d52ec8d6b5634
DominicL3 <leducd98@gmail.com>
2019-10-18 00:40:40 -0700

    copied over relevant parts of psr2np to test speed

--- simulateFRBclassification/tests/psr2np_speed_test.py
+++ simulateFRBclassification/tests/psr2np_speed_test.py
@@ -1,1 +1,95 @@
-psr2np_speed
+#!/usr/bin/python
+
+# import psrchive from Vishal's path
+import imp
+psr = imp.load_source('psrchive', '/home/vgajjar/linux64_bin/lib/python2.7/site-packages/psrchive.py')
+
+from tqdm import tqdm
+import numpy as np
+import argparse
+import glob
+
+"""
+Takes a directory of .ar files and converts them into one 
+large numpy array with dimensions (num_samples, NCHAN, 256). 
+The number of frequency channels will be scrunched to NCHAN, 
+and dispersion measure is randomized with every sample.
+
+NOTE: since chop_off() splits the arrays into chunks, the number 
+of samples actually generated is the number of samples given as a 
+command line argument multiplied by the number of chunks that 
+chop_off() has to split the arrays into to get 256 time bins.
+
+"""
+
+def psr2np(fname, NCHAN, dm):
+    # Get psrchive file as input and outputs numpy array
+    fpsr = psr.Archive_load(fname)
+
+    # must disperse the signal then dedisperse due to crashes on already dedispersed signals
+    fpsr.dededisperse()
+    fpsr.set_dispersion_measure(dm)
+    fpsr.dedisperse()
+
+    # resize image to number of frequency channels
+    fpsr.fscrunch_to_nchan(NCHAN)
+    fpsr.remove_baseline()
+
+    # -- apply weights for RFI lines --#
+    ds = fpsr.get_data().squeeze()
+    
+    # set channels marked as RFI (zero weight) to NaN
+    w = fpsr.get_weights().flatten()
+    w = w / np.max(w)
+    idx = np.where(w == 0)[0]
+    ds = np.multiply(ds, w[np.newaxis, :, np.newaxis])
+    ds[:, idx, :] = np.nan
+
+    # -- Get total intensity data (I) from the full stokes --#
+    data = ds[0, :, :]
+
+    # -- Get frequency axis values --#
+    freq = np.linspace(fpsr.get_centre_frequency() - abs(fpsr.get_bandwidth() / 2),
+                       fpsr.get_centre_frequency() + abs(fpsr.get_bandwidth() / 2), fpsr.get_nchan())
+
+    return data, w, freq
+
+if __name__== "__main__":
+    # Read command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_RFI', type=str)
+    parser.add_argument('--num_samples', type=int, default=320, help='Number of RFI arrays to generate')
+    parser.add_argument('--save_name', type=str, default='psr_arrays.npz',
+                        help='Filename to save frequency-time arrays')
+    
+    parser.add_argument('--NCHAN', type=int, default=64,
+                        help='Number of frequency channels to resize psrchive files to')
+    
+    parser.add_argument('--min_DM', type=float, default=0.0, help='Minimum DM to sample')
+    parser.add_argument('--max_DM', type=float, default=1000.0, help='Maximum DM to sample')
+    
+    args = parser.parse_args()
+
+    path = args.path_RFI
+    save_name = args.save_name
+    NCHAN = args.NCHAN
+
+    files = glob.glob(path + "*.ar" if path[:-1] == '/' else path + '/*.ar')
+    print("Length of files: %d" % len(files))
+
+    if not files:
+        raise ValueError("No files found in path " + path)
+
+    # choose DM and files from a uniform distribution
+    random_DMs = np.random.uniform(low=args.min_DM, high=args.max_DM, size=args.num_samples)
+    random_files = np.random.choice(files, size=args.num_samples, replace=True)
+
+    print("Unique number of random files: %d" % len(np.unique(files)))
+
+    # transform .ar files into numpy arrays and time how long it took
+    psrchive_data, weights = [], []
+    
+    for filename, DM in tqdm(zip(random_files, random_DMs), total=len(random_files)):
+        data, w, freq = psr2np(filename, NCHAN, DM)
+        psrchive_data.append(data)
+        weights.append(w)
