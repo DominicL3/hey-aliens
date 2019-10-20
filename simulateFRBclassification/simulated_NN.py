5b302f8aba1723634ce862af7bc7a5a70e06212d
DominicL3 <leducd98@gmail.com>
2019-10-20 02:20:18 -0700

    normalize the background prior to injecting FRB

--- simulateFRBclassification/simulated_NN.py
+++ simulateFRBclassification/simulated_NN.py
@@ -234,8 +234,8 @@ class SimulatedFRB(object):
         self.sample_SNR(SNRmin, SNR_sigma, SNRmax) # get random SNR
         
         # add to normalized background
-        frb_with_background = self.injectFRB(SNR=self.SNR, background=background, weights=weights)
-        self.simulatedFRB = self.normalize_background(frb_with_background)
+        background = self.normalize_background(background)
+        self.simulatedFRB = self.injectFRB(SNR=self.SNR, background=background, weights=weights)
 
 def construct_conv2d(train_data, train_labels, eval_data, eval_labels, 
                      nfreq=64, ntime=256, epochs=32, n_dense1=256, n_dense2=128,
