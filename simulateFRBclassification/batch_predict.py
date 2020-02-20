122528919df75d524d2c25bbd5941b8b4ee01b0a
DominicL3 <leducd98@gmail.com>
2020-02-20 10:36:59 -0800

    remove whitespace characters

--- simulateFRBclassification/batch_predict.py
+++ simulateFRBclassification/batch_predict.py
@@ -6,6 +6,8 @@ txt_file = '/datax/scratch/vgajjar/Test_pipeline/A00_Cband_files'
 
 with open(txt_file) as f:
     fil_files = f.readlines()
+    fil_files = [x.strip() for x in fil_files]
+
 
 MJDs = [sp.check_output(["header", fil_file, "-tstart"]) for fil_file in fil_files]
 
