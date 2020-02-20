072015bb77b8470671b53510bd6cdd35f36d6775
DominicL3 <leducd98@gmail.com>
2020-02-20 10:42:04 -0800

    testing printing spandak MJDs

--- simulateFRBclassification/batch_predict.py
+++ simulateFRBclassification/batch_predict.py
@@ -6,9 +6,15 @@ txt_file = '/datax/scratch/vgajjar/Test_pipeline/A00_Cband_files'
 
 with open(txt_file) as f:
     fil_files = f.readlines()
-    fil_files = [x.strip() for x in fil_files]
+    fil_files = [x.strip() for x in fil_files] # remove whitespace characters
 
 
 MJDs = [sp.check_output(["header", fil_file, "-tstart"]) for fil_file in fil_files]
+MJDs = [x.strip() for x in MJDs] # remove whitespace characters
+
+for mjd in MJDs:
+    split = mjd.split('.')
+    spandak_numbers = split[0] + '_' + split[1][:4]
+    print(spandak_numbers)
 
 print(MJDs)
