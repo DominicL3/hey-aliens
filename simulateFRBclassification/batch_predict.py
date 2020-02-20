5dbe141decdf6c5170a6b2702cf1ac49cc3595a5
DominicL3 <leducd98@gmail.com>
2020-02-20 10:35:59 -0800

    change to use check_output

--- simulateFRBclassification/batch_predict.py
+++ simulateFRBclassification/batch_predict.py
@@ -7,7 +7,6 @@ txt_file = '/datax/scratch/vgajjar/Test_pipeline/A00_Cband_files'
 with open(txt_file) as f:
     fil_files = f.readlines()
 
-MJDs = [sp.Popen(["header", fil_file, "-tstart"], stdout=sp.PIPE, shell=True).communicate()
-            for fil_file in fil_files]
+MJDs = [sp.check_output(["header", fil_file, "-tstart"]) for fil_file in fil_files]
 
 print(MJDs)
