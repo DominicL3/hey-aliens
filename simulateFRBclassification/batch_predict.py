888e053e5e1f4d8bb6a5f7c8e7ba66f431688009
DominicL3 <leducd98@gmail.com>
2020-02-20 10:33:32 -0800

    put interpreter up at top

--- simulateFRBclassification/batch_predict.py
+++ simulateFRBclassification/batch_predict.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import os
 import subprocess as sp
 
@@ -5,7 +6,6 @@ txt_file = '/datax/scratch/vgajjar/Test_pipeline/A00_Cband_files'
 
 with open(txt_file) as f:
     fil_files = f.readlines()
-
 
 MJDs = [sp.Popen(["header", fil_file, "-tstart"], stdout=sp.PIPE, shell=True).communicate()
             for fil_file in fil_files]
