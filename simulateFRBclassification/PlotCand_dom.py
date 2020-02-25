9393ea95164e7339f32eab93409637a56a40dbb8
DominicL3 <leducd98@gmail.com>
2020-02-25 11:38:35 -0800

    place candidate in directory with FRBcand file, labeling with index, timestamp, and DM

--- simulateFRBclassification/PlotCand_dom.py
+++ simulateFRBclassification/PlotCand_dom.py
@@ -183,7 +183,8 @@ def plotParaCalc(snr,filter,dm,fl,fh,tint,nchan):
 
         return tbin,fbin,extime,frac,cand_band_smear
 
-def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,kill_chans,source_name,nchan,mask_file,smooth,zerodm,csv_file,save_png):
+def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,kill_chans,source_name,nchan,
+                        mask_file,smooth,zerodm,csv_file,save_png,frbcand_dir):
 	parallel=1
 	if(frb_cands.size >= 1 and noplot is not True):
 			if(frb_cands.size>1):
@@ -233,7 +234,8 @@ def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,ki
                                 if(any(l<=time<=u for (l,u) in kill_time_range) or extime < 0.0):
 					print "Candidate inside bad-time range"
 				else:
-					candname = '%04d' % (indx) + "_" + '%.3f' % (time) + "sec_DM" + '%.2f.png' % (dm)
+                                        # place candidate in directory with FRBcand file, labeling with index, timestamp, and DM
+					candname = os.path.dirname(frbcand_dir) + '%04d' % (indx) + "_" + '%.3f' % (time) + "sec_DM" + '%.2f' % (dm)
 					cmd = "python /home/dleduc/hey-aliens/simulateFRBclassification/waterfaller_vg_dom.py --show-ts " + \
 					       " -t " + str(TotDisplay) + \
 				 	       " --colour-map=viridis " + \
