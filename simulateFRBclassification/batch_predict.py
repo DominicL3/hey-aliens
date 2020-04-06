b43d479e237dea68eafc069c938291409555d71a
DominicL3 <leducd98@gmail.com>
2020-04-06 12:28:46 -0700

    newline added when there was no new line in cmd

--- simulateFRBclassification/batch_predict.py
+++ simulateFRBclassification/batch_predict.py
@@ -26,7 +26,7 @@ for i, fil_file in enumerate(fil_files):
         # wherever the FRBcand file is (probably spandak_dir)
         cmd = "python predict.py" + \
             " {0} {1} {2} ".format(model, fil_file, path_to_FRBcand) + \
-            "--save_predicted_FRBs /datax/scratch/dleduc/predicted_FRBs/{}".format('BLGCsurvey_Cband_A00_' + split[0] + '_' + split[1][:4]) + \
+            "--save_predicted_FRBs /datax/scratch/dleduc/predicted_FRBs/{}".format('BLGCsurvey_Cband_A00_' + split[0] + '_' + split[1][:4])
 
         # execute the command
         print('Predicting on file {0} / {1}'.format(i+1, len(fil_files)))
