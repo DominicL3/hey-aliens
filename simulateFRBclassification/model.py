68d7050d90751876d2dd77111de3cc55089cd7c9
DominicL3 <leducd98@gmail.com>
2020-04-29 14:38:35 -0700

    added padding='same' to convolutional layers

--- simulateFRBclassification/model.py
+++ simulateFRBclassification/model.py
@@ -40,13 +40,13 @@ def construct_conv2d(nfreq, ntime, num_conv_layers=2, num_filters=32):
 
     # create num_filters convolution filters, each of size 3x3
     # max pool to reduce the dimensionality
-    cnn_2d.add(Conv2D(num_filters, (3, 3), activation='relu', input_shape=(nfreq, ntime, 1)))
+    cnn_2d.add(Conv2D(num_filters, (3, 3), padding='same', activation='relu', input_shape=(nfreq, ntime, 1)))
     cnn_2d.add(MaxPooling2D(pool_size=(2, 2)))
 
     # repeat and double the filter size for each convolutional block to make this DEEP
     for layer_number in np.arange(num_conv_layers - 1):
         num_filters *= 2
-        cnn_2d.add(Conv2D(num_filters, (2, 2), activation='relu'))
+        cnn_2d.add(Conv2D(num_filters, (3, 3), padding='same', activation='relu'))
         cnn_2d.add(MaxPooling2D(pool_size=(2, 2)))
 
     # max pool all feature maps
@@ -78,12 +78,12 @@ def construct_time_cnn(ntime, num_conv_layers=2, num_filters=32):
     # create num_filters convolution filters, each of size 2x2
     # average pool to reduce the dimensionality
     # stride 1 and NO pooling because the signal spike is very short
-    time_cnn.add(Conv1D(num_filters, 1, activation='relu', input_shape=(ntime, 1)))
+    time_cnn.add(Conv1D(num_filters, 1, padding='same', activation='relu', input_shape=(ntime, 1)))
 
     # repeat and double the filter size for each convolutional block to make this DEEP
     for layer_number in np.arange(num_conv_layers - 1):
         num_filters *= 2
-        time_cnn.add(Conv1D(num_filters, 2, activation='relu'))
+        time_cnn.add(Conv1D(num_filters, 2, padding='same', activation='relu'))
 
     # max pool all feature maps
     time_cnn.add(GlobalMaxPooling1D())
